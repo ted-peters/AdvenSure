7f01623da0c59d05358cc124753962ddf30def12
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-11-28 18:35:44 -0500

    user.js added/authentication has been added / and new post routes have been drawn up for passport.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,31 @@
 const path= require('path');
 // express router
 const router = require('express').Router();
+const passport = require("passport");
 //link api
 
 router.use(function(req, res){
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
 });
+
+router.post("/register_login", (req, res, next) => {
+    passport.authenticate("local", function(err, user, info) {
+        if (err) {
+            return res.status(400).json({ errors: err });
+        }
+        if (!user) {
+            return res.status(400).json({ errors: "No user found" });
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                return res.status(400).json({ errors: err });
+            }
+            return res.status(200).json({ success: `logged in ${user.id}` });
+        });
+    })(req, res, next);
+});
+
+module.exports = router;
+
 
 module.exports = router;
