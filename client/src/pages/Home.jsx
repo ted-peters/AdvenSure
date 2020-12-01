2b29dea96b74c7a37e2bf9351d76bb2d3c8126ba
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-01 10:30:28 -0600

    homepage design elements

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Login from './Login'
+import SignUp from './SignUp'
+
+
 
 export default function Home() {
     return (
-        <div>
-            This is the HOME PAGE.
+        <div className = "homeBody">
+            <Login></Login>
+            <SignUp></SignUp>
         </div>
     )
 }
