d20fe162044fe12a489b277b08897e28127fc9b4
Halle Wehmeyer <hallewehmeyer@Halles-MBP.attlocal.net>
2020-12-03 12:52:37 -0600

    user page updated

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import React, {useState} from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-  Link
+  Link,
 } from "react-router-dom";
+import axios from 'axios';
 
 const Example = (props) => {
+  const[list, setList] = useState([]);
+
+  const userTrip = () => {
+    let title = document.getElementById('yourTrip').value;
+    const arr = [...list];
+        arr.push(title);
+        setList(arr);
+    // axios({
+    //   method: "POST",
+    //   data: {
+    //     startDate: document.getElementById('tripStart').value,
+    //     endDate: document.getElementById('tripReturn').value,
+    //     title: document.getElementById('yourTrip').value,
+    //   },
+    //   withCredentials: true,
+    //   url: "/api/user",
+    // });
+  };
   return (
     <div className="container">
       <div className="row">
@@ -21,39 +40,45 @@ const Example = (props) => {
           <h3>Create New Trip</h3>
           <Form>
             <FormGroup>
-              <Label for="exampleText">Text Area</Label>
-              <Input type="text" name="text" id="exampleText" />
+              <Label for="yourTrip">Your Trip</Label>
+              <Input type="text" name="text" id="yourTrip" />
             </FormGroup>
             <FormGroup>
-              <Label for="exampleDate">Trip Start Date</Label>
+              <Label for="tripStart">Trip Start Date</Label>
               <Input
                 type="date"
                 name="date"
-                id="exampleDate"
+                id="tripStart"
                 placeholder="date placeholder"
               />
             </FormGroup>
             <FormGroup>
-              <Label for="exampleDate">Trip Return Date</Label>
+              <Label for="tripReturn">Trip Return Date</Label>
               <Input
                 type="date"
                 name="date"
-                id="exampleDate"
+                id="tripReturn"
                 placeholder="date placeholder"
               />
             </FormGroup>
-            <Button>Create Checklist</Button>
+            <Button onClick={userTrip}>Create Checklist</Button>
           </Form>
         </div>
         <div className="col-3">
-          <Button className="btn btn-block btn-sm btn-light"><Link to="/weather">Destination Weather Condition</Link></Button>
+          <Button className="btn btn-block btn-sm btn-light">
+            <Link to="/weather">Destination Weather Condition</Link>
+          </Button>
           <hr />
           <h3>User Checklist</h3>
-          <ul className="userChecklistItem"></ul>
+          <ul className="userChecklistItem">
+            {list.map(item => (
+              <li><Button className="btn btn-block btn-sm btn-dark"><Link to="/checklist">{item}</Link></Button></li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Example;
