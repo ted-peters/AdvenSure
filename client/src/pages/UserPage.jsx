467c787ccf085cc4a2065040822f1077a206c39e
Halle Wehmeyer <hallewehmeyer@Halles-MBP.attlocal.net>
2020-12-03 10:55:20 -0600

    created user page trip info input and date selector/connected to checklist

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,135 +1,58 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
 
 const Example = (props) => {
   return (
-    <Form>
-      
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="examplePassword"
-          placeholder="password placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleUrl">Url</Label>
-        <Input
-          type="url"
-          name="url"
-          id="exampleUrl"
-          placeholder="url placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleNumber">Number</Label>
-        <Input
-          type="number"
-          name="number"
-          id="exampleNumber"
-          placeholder="number placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleDatetime">Datetime</Label>
-        <Input
-          type="datetime"
-          name="datetime"
-          id="exampleDatetime"
-          placeholder="datetime placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleDate">Date</Label>
-        <Input
-          type="date"
-          name="date"
-          id="exampleDate"
-          placeholder="date placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleTime">Time</Label>
-        <Input
-          type="time"
-          name="time"
-          id="exampleTime"
-          placeholder="time placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleColor">Color</Label>
-        <Input
-          type="color"
-          name="color"
-          id="exampleColor"
-          placeholder="color placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSearch">Search</Label>
-        <Input
-          type="search"
-          name="search"
-          id="exampleSearch"
-          placeholder="search placeholder"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-        <Input type="select" name="select" id="exampleSelect">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelectMulti">Select Multiple</Label>
-        <Input
-          type="select"
-          name="selectMulti"
-          id="exampleSelectMulti"
-          multiple
-        >
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleText">Text Area</Label>
-        <Input type="textarea" name="text" id="exampleText" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleFile">File</Label>
-        <Input type="file" name="file" id="exampleFile" />
-        <FormText color="muted">
-          This is some placeholder block-level help text for the above input.
-          It's a bit lighter and easily wraps to a new line.
-        </FormText>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleRange">Range</Label>
-        <Input type="range" name="range" id="exampleRange" />
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input type="radio" /> Option one is this and that—be sure to
-          include why it's great
-        </Label>
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" /> Check me out
-        </Label>
-      </FormGroup>
-    </Form>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 py-5">
+          <h1>Welcome USER</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-9">
+          <h3>Create New Trip</h3>
+          <Form>
+            <FormGroup>
+              <Label for="exampleText">Text Area</Label>
+              <Input type="text" name="text" id="exampleText" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleDate">Trip Start Date</Label>
+              <Input
+                type="date"
+                name="date"
+                id="exampleDate"
+                placeholder="date placeholder"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleDate">Trip Return Date</Label>
+              <Input
+                type="date"
+                name="date"
+                id="exampleDate"
+                placeholder="date placeholder"
+              />
+            </FormGroup>
+            <Button>Create Checklist</Button>
+          </Form>
+        </div>
+        <div className="col-3">
+          <Button className="btn btn-block btn-sm btn-light"><Link to="/weather">Destination Weather Condition</Link></Button>
+          <hr />
+          <h3>User Checklist</h3>
+          <ul className="userChecklistItem"></ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- routes/apiRoutes/trip.js
+++ routes/apiRoutes/trip.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const tripController = require('../../controllers/tripcontroller');
 
-router.route('/').get(tripController.findAll).post(tripController.create);
+router.route('/user').get(tripController.findAll).post(tripController.create);
 
 module.exports = router;
