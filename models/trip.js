13539d53bc87c546ecafda016e3bb5fa3a0b15d0
EJ <elizakennelly@gmail.com>
2020-12-05 00:12:48 -0600

    updated user models to include trip and checklist

--- models/trip.js
+++ models/trip.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const newTrip = new Schema({
-    _id: { type: String},
-    startDate: { type: String},
-    endDate: { type: String},
-    title: { type: String}
-});
+// const newTrip = new Schema({
+//     _id: { type: String},
+//     startDate: { type: String},
+//     endDate: { type: String},
+//     title: { type: String}
+// });
 
-const Trip = mongoose.model("Trip", newTrip);
+// const Trip = mongoose.model("Trip", newTrip);
 
-module.exports = Trip;
+// module.exports = Trip;
--- models/user.js
+++ models/user.js
@@ -9,6 +9,20 @@ const user = new mongoose.Schema({
   password: {
     type: String,
   },
+  trip: [{ type: mongoose.Schema.Types.ObjectId, ref: "Trip"}]
 });
-
+const trip = new mongoose.Schema({
+    startDate: { type: String},
+    endDate: { type: String},
+    title: { type: String},
+    user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    checklist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Checklist"}]
+})
+const checklist = new mongoose.Schema({
+  name: {type: String},
+  listItems: {type: Array},
+  trip: [{ type: mongoose.Schema.Types.ObjectId, ref: "Trip"}]
+})
 module.exports = mongoose.model("User", user);
+module.exports = mongoose.model("Trip", trip);
+module.exports = mongoose.model("Checklist", checklist);
